04cbf7a60173c73fef063fe7b9396622838e2b72
velvet <crescentvelvet3@gmail.com>
2026-08-07 17:41:55 +0800

    wan22_rotate：本地 whl 加 triton-3.2.0

--- wan22_rotate/00_setup_env.sh
+++ wan22_rotate/00_setup_env.sh
@@ -63,6 +63,7 @@ if [ "${INSTALL_DEPS:-0}" = "1" ]; then
         "$WAN_MODEL_DIR/nvidia_cublas_cu12-12.4.5.8-py3-none-manylinux2014_x86_64.whl" \
         "$WAN_MODEL_DIR/nvidia_cudnn_cu12-9.1.0.70-py3-none-manylinux2014_x86_64.whl" \
         "$WAN_MODEL_DIR/nvidia_cufft_cu12-11.2.1.3-py3-none-manylinux2014_x86_64.whl" \
+        "$WAN_MODEL_DIR/triton-3.2.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl" \
         "$WAN_MODEL_DIR/nvidia_cusparse_cu12-12.3.1.170-py3-none-manylinux2014_x86_64.whl" \
         "$WAN_MODEL_DIR/nvidia_cusparselt_cu12-0.6.2-py3-none-manylinux2014_x86_64.whl" \
         "$WAN_MODEL_DIR/nvidia_nccl_cu12-2.21.5-py3-none-manylinux2014_x86_64.whl" \
